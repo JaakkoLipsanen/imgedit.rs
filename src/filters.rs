8e495c939fde19c0b60d6df75f2d3e918bd4c664
Jaakko Lipsanen <jaakko.lipsanen@reaktor.fi>
2019-03-16 23:20:49 +0200

    Add tests for parse filters function

--- src/filters.rs
+++ src/filters.rs
@@ -2,7 +2,7 @@
 
 use std::error::Error;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Filter {
     Identity,
     InvertColor,
@@ -39,4 +39,33 @@ pub fn parse_filters(filters: Vec<String>) -> Result<Vec<Filter>, Box<Error>> {
             }
         })
         .collect();
+}
+
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_parse_filters_simple() {
+        assert_eq!(parse_filters(vec!["identity".to_string(), "invert-color".to_string()]).unwrap(), vec!(Filter::Identity, Filter::InvertColor))
+    }
+
+    #[test]
+    fn test_parse_filters_with_arg() {
+        assert_eq!(parse_filters(vec!["identity".to_string(), "brighten=0.5".to_string()]).unwrap(), vec!(Filter::Identity, Filter::Brighten { amount: 0.5 }))
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_parse_filters_invalid() {
+        parse_filters(vec!["non_existing_filter".to_string()]).unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_parse_filter_brighten_out_of_range() {
+        parse_filters(vec!["brighten=1.1".to_string()]).unwrap();
+    }
 }
